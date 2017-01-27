48178bbe67cd9ad7f9efb3a36cc058aa2535a3b9
Andrew <andrewbainter13@augustana.edu>
2017-01-27 11:59:12 -0600

    desktoplauncher

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.MyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.useGL30=true;
+		config.height=800;
+		config.width=1280;
 		new LwjglApplication(new MyGame(), config);
 	}
 }
